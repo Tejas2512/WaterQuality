b39148a341748b083abbfdf445fd0af80e474088
Tejas Dadhaniya <60884889+Tejas2512@users.noreply.github.com>
2021-07-27 18:20:47 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -2,6 +2,11 @@ try:
     import pytest
     import unittest
     from app import app
+    import pickle, os
+    import numpy as np
+    from unittest.mock import patch
+    import requests
+    import json
 
 except Exception as e:
     print("Some modules are missing {}".format(e))
@@ -17,11 +22,31 @@ class FlaskTest(unittest.TestCase):
         self.assertEqual(statuscode, 200)
 
     # test for content type
-    def test_contest(self):
+    def test_content(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        print(response.content_type, 'text/html; charset=utf-8')
-    
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+
+    # test for post request data
+    @patch('requests.post')
+    def test_post_request(self, mock_post):
+        info = {'ph value': 1, 'Hardness': 1, 'Solids': 1, 'Chloramines': 1, 'Sulfate':1, 'Conductivity':1, 'Organic carbon':1, 'Trihalomethanes':1, 'Turbidity':1}
+        resp = requests.post("/", data=json.dumps(info), headers={'Content-Type': 'application/json'})
+        mock_post.assert_called_with("/", data=json.dumps(info),
+                                     headers={'Content-Type': 'application/json'})
+
+    # test for models
+    def test_model(self):
+        val = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        model_path = os.path.join('models', 'xgboost.sav')
+        scaler_path = os.path.join('models', 'scaler.sav')
+        model = pickle.load(open(model_path, 'rb'))
+        scc = pickle.load(open(scaler_path, 'rb'))
+        data = scc.transform([val])
+        # make a prediction for the given data
+        res = model.predict(data)
+        self.assertEqual(np.array([0]), res)
+
 
 if __name__ == "__main__":
     unittest.main()
